30c7b6d6dd47d1a78512599579adb6d83b0c3f91
martinshenry99 <martinshenry149@gmail.com>
2025-08-31 00:24:26 +0000

    auto-commit for 1e92f25c-1733-4381-ae7e-ce76586cf8b7

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, APIRouter, HTTPException, Depends, File, UploadFile, Form, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel, EmailStr, Field
 from typing import List, Optional, Dict, Any
